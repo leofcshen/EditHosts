cddda51ca204c045494387ceb5692fb07ff4dc7b
Leo Shen <rovingwind93@gmail.com>
2026-01-12 10:44:33 +0800

    update

---
+++ EditHosts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace EditHosts;
+
+internal class Program {
+  static void Main(/*string[] args*/) {
+    SV_Process.RunAsAdmin("notepad.exe", @"C:\Windows\System32\drivers\etc\hosts");
+  }
+}
+
+public class SV_Process {
+  public static void RunAsAdmin(string fileName, string? arguments = null) {
+    var processInfo = new ProcessStartInfo {
+      FileName = fileName,
+      Arguments = arguments,
+      // 以管理員身份運行
+      Verb = "runas",
+      // 必須設為 true 才能使用 "runas"
+      UseShellExecute = true
+    };
+
+    Process.Start(processInfo);
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EditHosts
+¶}±Ò C:\Windows\System32\drivers\etc\hosts
